6aee6e03e68049fa96e5662a1948c42bfeff1a43
maayanch23-meet <maayanchorin@gmail.com>
2023-02-23 23:50:37 +0200

    ..

--- app/src/main/java/com/example/firstlab/HomeActivity.java
+++ app/src/main/java/com/example/firstlab/HomeActivity.java
@@ -1,19 +1,34 @@
 package com.example.firstlab;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+    private TextView welcome;
+    private ListView listView;
+    private ArrayList<User> users;
+    private ArrayAdapter<User> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        welcome = findViewById(R.id.welcome);
+        listView = findViewById(R.id.users_list);
+        users = new ArrayList<>();
+        users.add(new User("maayan","maayan@gmail","322749"));
+        arrayAdapter = new UserArrayAdapter(this, R.layout.user_row, users);
+        listView.setAdapter(arrayAdapter);
     }
 
     @Override
